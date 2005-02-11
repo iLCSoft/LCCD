f05536514cb0f937b6d9802b9c83862e516a1b67
Frank Gaede <frank.gaede@desy.de>
2005-02-11 15:34:13 +0000

    added member function pointer for function returning key

--- source/include/lccd/ConditionsMap.hh
+++ source/include/lccd/ConditionsMap.hh
@@ -7,14 +7,16 @@
 #include "lccd/IConditionsChangeListener.hh"
 
 #include <map>
+#include <typeinfo>
 
 namespace lccd {
   
-
-  /** Teplate class for maps of conditions data.
+  /** Teplate class for maps of conditions data. The class LCCONDOBJECT
+   *  has to have a constructor that takes an LCObject* as argument.
+   *  Typically this is the case for all LCGenericObject subclasses.
    * 
    *  @author F.Gaede, DESY
-   *  @version $Id: ConditionsMap.hh,v 1.1 2005-02-11 12:31:52 gaede Exp $
+   *  @version $Id: ConditionsMap.hh,v 1.2 2005-02-11 15:34:13 gaede Exp $
    */
   template <class KEY, class LCCONDOBJECT>
   class ConditionsMap : public lccd::IConditionsChangeListener {
@@ -23,6 +25,16 @@ namespace lccd {
     
     typedef typename std::map< KEY, LCCONDOBJECT >::iterator MapIter ;
 
+    // PMF: pointer to member function that returns the key
+    typedef KEY (LCCONDOBJECT::*PMF)() ;  
+
+
+    /** Only constructor: provide the pointer to the member fuction that returns the key*/
+    ConditionsMap( PMF pmf ) : _pmf(pmf) {
+    }
+
+
+    /** The d'tor.*/
     virtual ~ConditionsMap() {} 
 
 
@@ -39,20 +51,23 @@ namespace lccd {
       // add all elements in the collection to the map
       for( int i=0; i< col->getNumberOfElements() ; i++ ){
 	
+	// this requires that the c'tor exists in LCCONDOBJECT !
 	LCCONDOBJECT obj( col->getElementAt( i ) ) ;
 	
-	// FIXME: cannot assume obj.getCellID() exists -> member function pointer in c'tor ...
-	_map.insert( std::make_pair( obj.getCellID() , obj)  ) ;
-
+	
+	_map.insert( std::make_pair( (obj.*_pmf)() , obj)  ) ;
+	// call member function for key  ^^^^^^^  ,e.g.
+	// _map.insert( std::make_pair( obj.getCellID() , obj)  ) ;
       }
 
-      print( std::cout ) ;
+//       print( std::cout ) ;
     }
 
     void print(  std::ostream& os ) {
       
       os << "ConditionsMap"  
-//  	 << typeid(KEY).name() << "," << typeid( LCCONDOBJECT ) << ">" 
+//    	 << std::typeid(keyType).name() << "," 
+//   	 << typeid( typename LCCONDOBJECT ).name() 
 	 << std::endl ;
       
       //   for( std::map< KEY, LCCONDOBJECT >::iterator it = _map.begin() ;
@@ -60,7 +75,9 @@ namespace lccd {
 	   it != _map.end() ;
 	   it++ ){
 	
-	os << "  key: " <<  it->first  << " - " << it->second.getGain() << std::endl ;
+	os << "  key: " <<  it->first  << " - [" << it->second.id() << "]" 
+	   << " - " << typeid( it->second ).name() 
+	   << std::endl ;
       }
       
     }
@@ -69,7 +86,7 @@ namespace lccd {
   protected:
     
     std::map< KEY, LCCONDOBJECT > _map ;
-
+    PMF _pmf ;
 };
 
 } ; //end namespace
