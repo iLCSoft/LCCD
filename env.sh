b5ea329b4b6ad29ed47bae1b5166907baa44e808
Frank Gaede <frank.gaede@desy.de>
2005-02-24 08:34:19 +0000

    doc updated

--- env.sh
+++ env.sh
@@ -8,14 +8,12 @@
 #####################################################
 
 # -- required
-#export LCIO=/opt/products/lcio/1.3.1
-export LCIO=~gaede/lcio/devel/v01-04beta
+export LCIO=/afs/desy.de/group/it/ilcsoft/lcio/lccdv00-01
 
 #-- comment out for production 
 export LCCDDEBUG=1
 
 #-- optionally build with CondDBMySQL support
-#export CondDBMySQL=/data/gaede/CondDBMySQL
 export CondDBMySQL=/afs/desy.de/group/it/ilcsoft/CondDBMySQL
 export MYSQL_PATH=/opt/products/mysql/3.23.58
 export LD_LIBRARY_PATH=$CondDBMySQL/lib
