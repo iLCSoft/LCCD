b2dfafd05f98a5549b03dd366d0718c1008bc9bd
Frank Gaede <frank.gaede@desy.de>
2005-09-09 08:09:37 +0000

    fixed seg fault with -O3: template specialization moved to class declaration

--- source/include/lccd/Xdr.hh
+++ source/include/lccd/Xdr.hh
@@ -16,9 +16,9 @@ namespace lccd {
    *  every data item starts at a 32-bit word boundary.
    * 
    *  @author F.Gaede, DESY
-   *  @version $Id: Xdr.hh,v 1.2 2005-02-17 15:45:53 gaede Exp $
+   *  @version $Id: Xdr.hh,v 1.3 2005-09-09 08:09:37 gaede Exp $
    */
-
+  
   class Xdr {
     
   public:
@@ -28,9 +28,9 @@ namespace lccd {
      */
     static void copy( unsigned const char* from, unsigned char* dest, 
 		      const int size, const int count);
-
-
-
+    
+    
+    
     /** Writes count objects of (simple) type T to the string s
      *  in Xdr format.
      */
@@ -50,7 +50,7 @@ namespace lccd {
       unsigned char* from = reinterpret_cast< unsigned char* > ( t )  ;
 
 //        std::cout << " tostream: 0x" << std::hex << *t  << std::dec << std::endl ; 
-
+      
       Xdr::copy( from , dest , sizeof( T ) , count  ) ;
       
     }
@@ -89,10 +89,91 @@ namespace lccd {
       
       return position + padlen ;
     }
-           
-  };
+    
+    
+    
+    /** Specialization for strings - count is ignored and taken to be 1.
+     */
+//     template<>
+    static void tostream( std::string& s , std::string* strVal, unsigned count ) {
+      
+      int  strSize = (int) strVal->size() ;
+      
+      int bytlen, padlen;
+      bytlen = strSize + sizeof( int )  ;
+      
+      padlen = (bytlen + 3) & 0xfffffffc ; 
+      
+      unsigned index = s.size() ;
+      
+      s.resize( index + padlen ) ;
+      
+      
+      // write the string's size first 
+      
+      unsigned char* dest = reinterpret_cast< unsigned char* > (  & s[ index ] ) ;
+      unsigned char* from = reinterpret_cast< unsigned char* > ( & strSize )  ;
+      
+      Xdr::copy( from , dest , sizeof( int ) , 1  ) ;
+      
+      
+      dest = reinterpret_cast< unsigned char* > (  & s[ index + sizeof( int ) ] ) ;
+      from = reinterpret_cast< unsigned char* > ( & strVal->operator[](0) )  ;
+      
+      //     std::cout << " tostream: - " << *strVal 
+      // 	      << " - lenght: " << strSize
+      // 	      << std::endl ;
+      
+      Xdr::copy( from , dest , 1 , strSize  ) ;
+      
+    }
+
   
-} ; //end namespace
+    /** Specialization for strings - count is ignored and taken to be 1.
+     *  @return the new position in the stream.
+     */ 
+//     template <>
+    static unsigned  fromstream( const std::string& s, unsigned index, 
+				 std::string* strVal, unsigned count ) {
+      
+      if( count == 0 )
+	return index ;
+      
+      
+      // read the string length first 
+      int strSize ;
+      unsigned const char* from = reinterpret_cast< unsigned const char* > (  & s[ index ] ) ;
+      unsigned char* dest = reinterpret_cast< unsigned char* > ( & strSize )  ;
+      
+      Xdr::copy( from , dest , sizeof( int ) , 1  ) ;
+      
+      index += sizeof( int ) ;
+      
+      
+      unsigned bytlen, padlen;
+      bytlen =  strSize ;
+      padlen = (bytlen + 3) & 0xfffffffc ; 
+      
+      
+      strVal->resize( strSize ) ;
+      
+      from = reinterpret_cast< unsigned const char* > (  & s[ index ] ) ;
+      dest = reinterpret_cast< unsigned char* > ( & strVal->operator[](0)  )  ;
+      
+      Xdr::copy( from , dest , 1 , strSize  ) ;
+      
+      //     std::cout << " fromstream: - " << *strVal 
+      // 	      << " - lenght: " << strSize
+      // 	      << " - [" << index + padlen << "] " 
+      // 	      << std::endl ;
+      
+      
+      return index + padlen ;
+    }
+    
+
+  };  // class Xdr
+}  //end namespace
 
 #endif // USE_CONDDB
 #endif // Xdr_h
